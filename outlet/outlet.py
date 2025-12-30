247f1f1e39a60e5341623a8765f9b405ab800aae
Atu120304 <atsivi1234@gmail.com>
2025-12-30 12:12:15 +0000

    Update outlet.py

--- outlet/outlet.py
+++ outlet/outlet.py
@@ -381,4 +381,10 @@ def handle_leave(room):
 
 
 if __name__ == "__main__":
-     app.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port
+    )
+
